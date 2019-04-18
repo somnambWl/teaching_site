08cea72f352c8701d29684e2bcc72163d58812be
somnambWl <somnambWl@gmail.com>
2019-04-18 12:34:56 +0200

    Added rate of success in percents. It is computed as the number of earned points divided by the number of all scored questions. Note that if you mark more exercises as scored and you do not open them, they will still count into success rate.

--- app/common/tables.py
+++ app/common/tables.py
@@ -9,7 +9,7 @@ from flask_table import Table, Col, create_table
 
 from sqlalchemy import func
 
-from app.models.exercise import Exercise, Sheet, Question
+from app.models.exercise import Exercise, Sheet, Question, question_exercise
 from app.models.user import User
 
 def create_summary_table():
@@ -26,17 +26,27 @@ def create_summary_table():
         Summary table
     """
     sql = db.session.query(User.fullname, Exercise.name,
-            func.sum(Sheet.point)).filter(Sheet.user_id==User.id)\
+            func.sum(Sheet.point))\
+            .filter(Sheet.user_id==User.id)\
             .filter(Sheet.exercise_id==Exercise.id)\
             .filter(User.enrolled==True)\
             .filter(Exercise.scored==True)\
             .group_by(User.fullname, Exercise.name)
+    sql2 = db.session.query(Question.id)\
+            .filter(Exercise.scored==True)\
+            .filter(question_exercise.c.exercise_id == Exercise.id)\
+            .filter(question_exercise.c.question_id == Question.id)\
+            .group_by(Question.id)
     try:
+        df2 = pd.read_sql(sql2.statement, db.session.bind)
+        number_of_exercises = len(df2)
         df = pd.read_sql(sql.statement, db.session.bind)
         df = df.pivot_table(values='sum_1', index="fullname", columns="name")
         df.index.name = None
         df.columns.name = None
         df["Total"] = df[list(df.columns)].sum(axis=1)
+        df["Success"] = df["Total"]/number_of_exercises*100
+        df["Success"] = df["Success"].round(2).astype("str")+" %"
         return df
     except:
         return None
